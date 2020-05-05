c7c5fb88b2b4ad76ac9d33a7556497d53854b701
Ashley Unitt <ashley@unitt.org.uk>
2020-05-05 10:54:57 +0100

    Updates

---
+++ precache-manifest.97cad5681ca41e33cb008add54309346.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce95b77b1224212e6b1e5d153f16af5b",
+    "url": "/sitigrid-app/index.html"
+  },
+  {
+    "revision": "b1d01c62379e40bde9b3",
+    "url": "/sitigrid-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "150140b4dd25739f196e",
+    "url": "/sitigrid-app/static/js/2.7ac14ce5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sitigrid-app/static/js/2.7ac14ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1d01c62379e40bde9b3",
+    "url": "/sitigrid-app/static/js/main.dbc383db.chunk.js"
+  },
+  {
+    "revision": "d7db176c9191e32b9fe5",
+    "url": "/sitigrid-app/static/js/runtime-main.e1f9be77.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sitigrid-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
