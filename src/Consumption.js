4bb3650d595514aba41c50db9f38a767d3914b72
Ashley Unitt <ashley@unitt.org.uk>
2020-05-06 13:58:45 +0100

    Pulled table styles into a separate file

--- src/Consumption.js
+++ src/Consumption.js
@@ -1,37 +1,12 @@
 import React, {Component} from 'react';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { ResponsiveContainer, AreaChart, Area, XAxis, YAxis } from 'recharts'
-
-const useStyles = makeStyles({
-    table: {
-      minWidth: 650,
-    },
-  });
-
-const StyledTableCell = withStyles((theme) => ({
-    head: {
-        backgroundColor: theme.palette.common.black,
-        color: theme.palette.common.white,
-    },
-    body: {
-        fontSize: 14,
-    },
-}))(TableCell);
-
-const StyledTableRow = withStyles((theme) => ({
-    root: {
-      '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.action.hover,
-      },
-    },
-  }))(TableRow);
+import { ResponsiveContainer, AreaChart, Area, XAxis, YAxis } from 'recharts';
+import { useStyles, StyledTableCell, StyledTableRow } from './tableStyles';
 
 class Consumption extends Component {
     constructor(props) {
